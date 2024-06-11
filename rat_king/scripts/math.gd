8e93423ac42e1ceb3126e485edea2931892877da
Friedrich Hanisch <friedrich@ratking.de>
2024-06-11 18:30:42 +0200

    add smooth damp function for Vector3 (stolen from Unity)

--- rat_king/scripts/math.gd
+++ rat_king/scripts/math.gd
@@ -93,21 +93,66 @@ static func vec3_frin_lerp(a: Vector3, b: Vector3, t: float, delta: float, hertz
 ### smooth damping
 
 # from https://github.com/Unity-Technologies/UnityCsReference/blob/master/Runtime/Export/Math/Mathf.cs
-static func smooth_damp(cur: float, target: float, cur_velocity: Array[float], smooth_time: float, delta_time: float, maxSpeed := INF) -> float:
+static func smooth_damp(cur: float, target: float, cur_velocity: Array[float], smooth_time: float, delta_time: float, max_speed := INF) -> float:
 	smooth_time = maxf(0.0001, smooth_time)
 	var omega := 2.0 / smooth_time
 	var x := omega * delta_time
 	var exp := 1.0 / (1.0 + x + 0.48 * x * x + 0.235 * x * x * x)
 	var original_to := target
-	var max_change := maxSpeed * smooth_time
+	var max_change := max_speed * smooth_time
 	var change := clampf(cur - target, -max_change, max_change)
 	target = cur - change
 	var temp := (cur_velocity[0] + omega * change) * delta_time
 	cur_velocity[0] = (cur_velocity[0] - omega * temp) * exp
 	var result := target + (change + temp) * exp
 	if (original_to - cur > 0.0) == (result > original_to):
-		result = original_to
 		cur_velocity[0] = 0.0
+		return original_to
 	return result
 	
-# TODO: https://github.com/Unity-Technologies/UnityCsReference/blob/master/Runtime/Export/Math/Vector3.cs
+# from https://github.com/Unity-Technologies/UnityCsReference/blob/master/Runtime/Export/Math/Vector3.cs
+static func vec3_smooth_damp(cur: Vector3, target: Vector3, cur_velocity: Array[Vector3], smooth_time: float, delta_time: float, max_speed := INF) -> Vector3:
+	var output_x := 0.0
+	var output_y := 0.0
+	var output_z := 0.0
+	smooth_time = maxf(0.0001, smooth_time)
+	var omega := 2.0 / smooth_time
+	var x := omega * delta_time
+	var exp := 1.0 / (1.0 + x + 0.48 * x * x + 0.235 * x * x * x)
+	var change_x := cur.x - target.x
+	var change_y := cur.y - target.y
+	var change_z := cur.z - target.z
+	var original_to := target
+	var max_change := max_speed * smooth_time
+	var max_change_sq := max_change * max_change
+	var sqr_mag := change_x * change_x + change_y * change_y + change_z * change_z
+	if sqr_mag > max_change_sq:
+		var mag := sqrt(sqr_mag)
+		change_x = change_x / mag * max_change
+		change_y = change_y / mag * max_change
+		change_z = change_z / mag * max_change
+	target.x = cur.x - change_x;
+	target.y = cur.y - change_y;
+	target.z = cur.z - change_z;
+	var temp_x := (cur_velocity[0].x + omega * change_x) * delta_time
+	var temp_y := (cur_velocity[0].y + omega * change_y) * delta_time
+	var temp_z := (cur_velocity[0].z + omega * change_z) * delta_time
+	cur_velocity[0].x = (cur_velocity[0].x - omega * temp_x) * exp
+	cur_velocity[0].y = (cur_velocity[0].y - omega * temp_y) * exp
+	cur_velocity[0].z = (cur_velocity[0].z - omega * temp_z) * exp
+	output_x = target.x + (change_x + temp_x) * exp
+	output_y = target.y + (change_y + temp_y) * exp
+	output_z = target.z + (change_z + temp_z) * exp
+	# prevent overshooting
+	var orig_minus_cur_x := original_to.x - cur.x
+	var orig_minus_cur_y := original_to.y - cur.y
+	var orig_minus_cur_z := original_to.z - cur.z
+	var out_minus_orig_x := output_x - original_to.x
+	var out_minus_orig_y := output_y - original_to.y
+	var out_minus_orig_z := output_z - original_to.z
+	if orig_minus_cur_x * out_minus_orig_x + orig_minus_cur_y * out_minus_orig_y + orig_minus_cur_z * out_minus_orig_z > 0:
+		cur_velocity[0].x = 0.0
+		cur_velocity[0].y = 0.0
+		cur_velocity[0].z = 0.0
+		return original_to
+	return Vector3(output_x, output_y, output_z)
