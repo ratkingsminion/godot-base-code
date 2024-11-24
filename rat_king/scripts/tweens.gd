7083f23653b07b73efae926c86d3ba42d137e6d7
Friedrich Hanisch <friedrich@ratking.de>
2024-11-24 08:47:12 +0100

    Add easing functions to Tweens script

--- rat_king/scripts/tweens.gd
+++ rat_king/scripts/tweens.gd
@@ -1,7 +1,7 @@
 class_name Tweens
 extends Node
 
-static var _tree: SceneTree
+static var _tree: SceneTree = Engine.get_main_loop()
 
 ###
 
@@ -78,3 +78,135 @@ static func tree_do_curve(start, end, duration: float, curve: Curve, on_update: 
 	var t := _tree.create_tween()
 	t.tween_method(func(f: float) -> void: on_update.call(lerp(start, end, curve.sample(f))), 0.0, 1.0, duration)
 	return t
+
+###
+
+# easings from: https://github.com/ai/easings.net/blob/master/src/easings/easingsFunctions.ts
+
+const _c1 := 1.70158
+const _c2 := _c1 * 1.525
+const _c3 := _c1 + 1
+const _c4 := (2.0 * PI) / 3.0
+const _c5 := (2.0 * PI) / 4.5
+
+static func _bounce_out(t: float) -> float:
+	var n1 := 7.5625
+	var d1 := 2.75
+	if t < 1.0 / d1:
+		return n1 * t * t
+	elif t < 2.0 / d1:
+		t -= 1.5 / d1
+		return n1 * t * t + 0.75
+	elif t < 2.5 / d1:
+		t -= 2.25 / d1
+		return n1 * t * t + 0.9375
+	t -= 2.625 / d1
+	return n1 * t * t + 0.984375
+
+static func in_quad(t: float) -> float:
+	return t * t
+
+static func out_quad(t: float) -> float:
+	return 1.0 - (1.0 - t) * (1.0 - t)
+
+static func in_out_quad(t: float) -> float:
+	return 2.0 * t * t if t < 0.5 else 1.0 - pow(-2.0 * t + 2.0, 2.0) * 0.5
+
+static func in_cubic(t: float) -> float:
+	return t * t * t
+
+static func out_cubic(t: float) -> float:
+	return 1.0 - pow(1.0 - t, 3.0)
+
+static func in_out_cubic(t: float) -> float:
+	return 4.0 * t * t * t if t < 0.5 else 1.0 - pow(-2.0 * t + 2.0, 3.0) * 0.5
+
+static func in_quart(t: float) -> float:
+	var tt := t * t
+	return tt * tt
+
+static func out_quart(t: float) -> float:
+	return 1.0 - pow(1 - t, 4.0)
+
+static func in_out_quart(t: float) -> float:
+	var tt := t * t
+	return 8.0 * tt * tt if t < 0.5 else 1.0 - pow(-2.0 * t + 2.0, 4.0) * 0.5
+
+static func in_quint(t: float) -> float:
+	var tt := t * t
+	return tt * tt * t
+
+static func out_quint(t: float) -> float:
+	return 1.0 - pow(1.0 - t, 5.0)
+
+static func in_out_quint(t: float) -> float:
+	var tt := t * t
+	return 16.0 * tt * tt * t if t < 0.5 else 1.0 - pow(-2.0 * t + 2.0, 5.0) * 0.5
+
+static func in_sin(t: float) -> float:
+	return 1.0 - cos((t * PI) * 0.5)
+
+static func out_sin(t: float) -> float:
+	return sin((t * PI) * 0.5)
+
+static func in_out_sin(t: float) -> float:
+	return -(cos(PI * t) - 1.0) * 0.5
+
+static func in_expo(t: float) -> float:
+	return 0.0 if t == 0.0 else pow(2.0, 10.0 * t - 10.0)
+
+static func out_expo(t: float) -> float:
+	return 1.0 if t == 1.0 else 1.0 - pow(2.0, -10.0 * t)
+
+static func in_out_expo(t: float) -> float:
+	return 0.0 if t == 0.0 \
+		else 1.0 if t == 1.0 \
+		else pow(2.0, 20.0 * t - 10.0) * 0.5 if t < 0.5 \
+		else (2.0 - pow(2.0, -20.0 * t + 10.0)) * 0.5
+
+static func in_circ(t: float) -> float:
+	return 1.0 - sqrt(1.0 - pow(t, 2.0))
+
+static func out_circ(t: float) -> float:
+	return sqrt(1.0 - pow(t - 1.0, 2.0))
+
+static func in_out_circ(t: float) -> float:
+	return (1.0 - sqrt(1 - pow(2.0 * t, 2.0))) * 0.5 if t < 0.5 \
+		else (sqrt(1.0 - pow(-2.0 * t + 2.0, 2.0)) + 1.0) * 0.5
+
+static func in_back(t: float) -> float:
+	var tt := t * t
+	return _c3 * tt * t - _c1 * tt
+
+static func out_back(t: float) -> float:
+	return 1.0 + _c3 * pow(t - 1.0, 3.0) + _c1 * pow(t - 1.0, 2.0)
+
+static func in_out_back(t: float) -> float:
+	return (pow(2.0 * t, 2.0) * ((_c2 + 1.0) * 2.0 * t - _c2)) * 0.5 if t < 0.5 \
+		else (pow(2.0 * t - 2.0, 2.0) * ((_c2 + 1.0) * (t * 2.0 - 2.0) + _c2) + 2.0) * 0.5
+
+static func in_elastic(t: float) -> float:
+	return 0.0 if t == 0.0 \
+		else 1.0 if t == 1.0 \
+		else -pow(2.0, 10.0 * t - 10.0) * sin((t * 10.0 - 10.75) * _c4)
+
+static func out_elastic(t: float) -> float:
+	return 0.0 if t == 0.0 \
+		else 1.0 if t == 1.0 \
+		else pow(2.0, -10.0 * t) * sin((t * 10.0 - 0.75) * _c4) + 1.0
+
+static func in_out_elastic(t: float) -> float:
+	return 0.0 if t == 0.0 \
+		else 1.0 if t == 1.0 \
+		else -(pow(2.0, 20.0 * t - 10.0) * sin((20.0 * t - 11.125) * _c5)) * 0.5 if t < 0.5 \
+		else (pow(2.0, -20.0 * t + 10.0) * sin((20.0 * t - 11.125) * _c5)) * 0.5 + 1.0
+
+static func in_bounce(t: float) -> float:
+	return 1.0 - _bounce_out(1.0 - t)
+
+static func out_bounce(t: float) -> float:
+	return _bounce_out(t)
+
+static func in_out_bounce(t: float) -> float:
+	return (1.0 - _bounce_out(1.0 - 2.0 * t)) * 0.5 if t < 0.5 \
+		else (1.0 + _bounce_out(2.0 * t - 1.0)) * 0.5
