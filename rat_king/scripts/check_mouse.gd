91c04e3e3f22b7cfdcc2de5507368c232281b7d0
Friedrich Hanisch <friedrich@ratking.de>
2026-02-23 19:45:32 +0100

    Update check_mouse.gd

--- rat_king/scripts/check_mouse.gd
+++ rat_king/scripts/check_mouse.gd
@@ -6,7 +6,7 @@ extends Control
 # Be aware that buttons always stop the mouse_entered propagation, so you
 # need to register them too
 
-static var over_counter: Dictionary
+static var over_counter: Array[Node]
 static var over_timer := 0
 
 ###
@@ -17,15 +17,18 @@ func _ready() -> void:
 ###
 
 static func is_over(time := 50) -> bool:
-	return over_counter.size() > 0 or over_timer + time > Time.get_ticks_msec()
+	if over_timer + time > Time.get_ticks_msec(): return true
+	if null in over_counter: over_counter = over_counter.filter(func(n: Node) -> bool: return n != null)
+	return over_counter.size()
 
 static func register(node: Node) -> void:
-	if node == null: print("check_mouse: trying to register null node"); return
+	if node == null: print("CheckMouse: trying to register null node"); return
 	var control := node as Control
 	if control == null:
-		control = Helpers.find_in_all_children(node, Control, true, true)
+		control = Helpers.find_in_all_children(node, Control, false, true)
+		#control = node.find_children("*", "Control").front()
 		if control == null:
-			print("check_mouse: node ", node.name, " is not a Control and has no Control child")
+			printerr("check_mouse: node ", node.name, " is not a Control and has no Control child")
 			return
 	
 	control.mouse_entered.connect(CheckMouse._add_over.bind(control))
@@ -35,6 +38,7 @@ static func register(node: Node) -> void:
 	
 	# go through the hierarchy and set the mouse filter to pass
 	for c in Helpers.get_all_children(node, false, true):
+	#for c in node.find_children("*"):
 		if c is Control:
 			if c.mouse_filter == Control.MOUSE_FILTER_STOP:
 				c.mouse_filter = Control.MOUSE_FILTER_PASS
@@ -44,11 +48,13 @@ static func register(node: Node) -> void:
 ### events
 
 static func _add_over(node: Node) -> void:
-	over_counter[node] = 0
+	if node not in over_counter:
+		over_counter.append(node)
 
 static func _remove_over(node: Node) -> void:
-	if over_counter.erase(node):
-		if over_counter.size() == 0:
+	if node in over_counter:
+		over_counter.erase(node)
+		if not over_counter:
 			over_timer = Time.get_ticks_msec()
 
 static func _remove_on_invisibility(obj: Control) -> void:
