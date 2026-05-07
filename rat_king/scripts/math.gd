15a22269bf3c2546de394cd2d22e783c3756999c
Friedrich Hanisch <friedrich@ratking.de>
2026-05-07 10:56:54 +0200

    Update math.gd

--- rat_king/scripts/math.gd
+++ rat_king/scripts/math.gd
@@ -5,36 +5,34 @@ class_name Math
 static func rnd_np() -> int:
 	return -1 if randf() < 0.5 else 1
 
+static func rnd_npf(scale := 1.0) -> float:
+	return -scale if randf() < 0.5 else scale
+
 ### numbers
 
 static func wrapi_array(t: int, array: Array) -> int:
 	if not array: return 0
 	return t - floor(t / float(array.size())) * array.size()
 
+static func clampf01(value: float) -> float:
+	return clampf(value, 0.0, 1.0)
+
 static func remap_clamped(value: float, min_in: float, max_in: float, min_out: float, max_out: float) -> float:
 	return remap(clampf(value, min_in, max_in), min_in, max_in, min_out, max_out)
 
-### directions
+static func inverse_lerp_clamped(from: float, to: float, value: float) -> float:
+	return clampf(inverse_lerp(from, to, value), 0.0, 1.0)
 
-static func get_right(node: Node3D) -> Vector3:
-	return node.get_global_transform().basis.x
-
-static func get_left(node: Node3D) -> Vector3:
-	return -node.get_global_transform().basis.x
-
-static func get_up(node: Node3D) -> Vector3:
-	return node.get_global_transform().basis.y
-
-static func get_down(node: Node3D) -> Vector3:
-	return -node.get_global_transform().basis.y
-
-static func get_forward(node: Node3D) -> Vector3:
-	return -node.get_global_transform().basis.z
-
-static func get_back(node: Node3D) -> Vector3:
-	return node.get_global_transform().basis.z
+static func lerpf_clamped(from: float, to: float, weight: float) -> float:
+	return lerpf(from, to, clampf(weight, 0.0, 1.0))
 	
 ### vectors
+
+static func vec3_to_vec2xz(vec: Vector3) -> Vector2:
+	return Vector2(vec.x, vec.z)
+
+static func vec3i_to_vec2ixz(vec: Vector3i) -> Vector2i:
+	return Vector2i(vec.x, vec.z)
 
 static func vec2_lerp(a: Vector2, b: Vector2, t: Vector2) -> Vector2:
 	return Vector2(lerpf(a.x, b.x, t.x), lerpf(a.y, b.y, t.y))
@@ -74,15 +72,28 @@ static func vec3_add_y(vec: Vector3, y: float) -> Vector3:
 static func vec3_add_z(vec: Vector3, z: float) -> Vector3:
 	return Vector3(vec.x, vec.y, vec.z + z)
 
+###
+
+static func vec3_deg_to_rad(vec: Vector3) -> Vector3:
+	return Vector3(deg_to_rad(vec.x), deg_to_rad(vec.y), deg_to_rad(vec.z))
+
 ### frame independent lerping
 
 static func frin_lerpf(a: float, b: float, t: float, delta: float, hertz := 60.0) -> float:
 	t = (1.0 - t) ** (delta * hertz)
 	return t * a + (1.0 - t) * b
 
+static func vec2_frin_lerp(a: Vector2, b: Vector2, t: float, delta: float, hertz := 60.0) -> Vector2:
+	t = (1.0 - t) ** (delta * hertz)
+	return Vector2(t * a.x + (1.0 - t) * b.x, t * a.y + (1.0 - t) * b.y)
+
 static func vec3_frin_lerp(a: Vector3, b: Vector3, t: float, delta: float, hertz := 60.0) -> Vector3:
 	t = (1.0 - t) ** (delta * hertz)
 	return Vector3(t * a.x + (1.0 - t) * b.x, t * a.y + (1.0 - t) * b.y, t * a.z + (1.0 - t) * b.z)
+
+static func color_frin_lerp(a: Color, b: Color, t: float, delta: float, hertz := 60.0) -> Color:
+	t = (1.0 - t) ** (delta * hertz)
+	return Color(t * a.r + (1.0 - t) * b.r, t * a.g + (1.0 - t) * b.g, t * a.b + (1.0 - t) * b.b, t * a.a + (1.0 - t) * b.a)
 
 ## uses radians
 static func frin_lerp_angle(a: float, b: float, t: float, delta: float, hertz := 60.0) -> float:
@@ -114,10 +125,42 @@ static func smooth_damp(cur: float, target: float, cur_velocity: Array[float], s
 	return result
 	
 # from https://github.com/Unity-Technologies/UnityCsReference/blob/master/Runtime/Export/Math/Vector3.cs
+static func vec2_smooth_damp(cur: Vector2, target: Vector2, cur_velocity: Array[Vector2], smooth_time: float, delta_time: float, max_speed := INF) -> Vector2:
+	smooth_time = maxf(0.0001, smooth_time)
+	var omega := 2.0 / smooth_time
+	var x := omega * delta_time
+	var expo := 1.0 / (1.0 + x + 0.48 * x * x + 0.235 * x * x * x)
+	var change_x := cur.x - target.x
+	var change_y := cur.y - target.y
+	var original_to := target
+	var max_change := max_speed * smooth_time
+	var max_change_sq := max_change * max_change
+	var sqr_mag := change_x * change_x + change_y * change_y
+	if sqr_mag > max_change_sq:
+		var mag := sqrt(sqr_mag)
+		change_x = change_x / mag * max_change
+		change_y = change_y / mag * max_change
+	target.x = cur.x - change_x
+	target.y = cur.y - change_y
+	var temp_x := (cur_velocity[0].x + omega * change_x) * delta_time
+	var temp_y := (cur_velocity[0].y + omega * change_y) * delta_time
+	cur_velocity[0].x = (cur_velocity[0].x - omega * temp_x) * expo
+	cur_velocity[0].y = (cur_velocity[0].y - omega * temp_y) * expo
+	var output_x := target.x + (change_x + temp_x) * expo
+	var output_y := target.y + (change_y + temp_y) * expo
+	# prevent overshooting
+	var orig_minus_cur_x := original_to.x - cur.x
+	var orig_minus_cur_y := original_to.y - cur.y
+	var out_minus_orig_x := output_x - original_to.x
+	var out_minus_orig_y := output_y - original_to.y
+	if orig_minus_cur_x * out_minus_orig_x + orig_minus_cur_y * out_minus_orig_y > 0.0:
+		cur_velocity[0].x = 0.0
+		cur_velocity[0].y = 0.0
+		return original_to
+	return Vector2(output_x, output_y)
+	
+# from https://github.com/Unity-Technologies/UnityCsReference/blob/master/Runtime/Export/Math/Vector3.cs
 static func vec3_smooth_damp(cur: Vector3, target: Vector3, cur_velocity: Array[Vector3], smooth_time: float, delta_time: float, max_speed := INF) -> Vector3:
-	var output_x := 0.0
-	var output_y := 0.0
-	var output_z := 0.0
 	smooth_time = maxf(0.0001, smooth_time)
 	var omega := 2.0 / smooth_time
 	var x := omega * delta_time
@@ -134,18 +177,18 @@ static func vec3_smooth_damp(cur: Vector3, target: Vector3, cur_velocity: Array[
 		change_x = change_x / mag * max_change
 		change_y = change_y / mag * max_change
 		change_z = change_z / mag * max_change
-	target.x = cur.x - change_x;
-	target.y = cur.y - change_y;
-	target.z = cur.z - change_z;
+	target.x = cur.x - change_x
+	target.y = cur.y - change_y
+	target.z = cur.z - change_z
 	var temp_x := (cur_velocity[0].x + omega * change_x) * delta_time
 	var temp_y := (cur_velocity[0].y + omega * change_y) * delta_time
 	var temp_z := (cur_velocity[0].z + omega * change_z) * delta_time
 	cur_velocity[0].x = (cur_velocity[0].x - omega * temp_x) * expo
 	cur_velocity[0].y = (cur_velocity[0].y - omega * temp_y) * expo
 	cur_velocity[0].z = (cur_velocity[0].z - omega * temp_z) * expo
-	output_x = target.x + (change_x + temp_x) * expo
-	output_y = target.y + (change_y + temp_y) * expo
-	output_z = target.z + (change_z + temp_z) * expo
+	var output_x := target.x + (change_x + temp_x) * expo
+	var output_y := target.y + (change_y + temp_y) * expo
+	var output_z := target.z + (change_z + temp_z) * expo
 	# prevent overshooting
 	var orig_minus_cur_x := original_to.x - cur.x
 	var orig_minus_cur_y := original_to.y - cur.y
@@ -153,7 +196,7 @@ static func vec3_smooth_damp(cur: Vector3, target: Vector3, cur_velocity: Array[
 	var out_minus_orig_x := output_x - original_to.x
 	var out_minus_orig_y := output_y - original_to.y
 	var out_minus_orig_z := output_z - original_to.z
-	if orig_minus_cur_x * out_minus_orig_x + orig_minus_cur_y * out_minus_orig_y + orig_minus_cur_z * out_minus_orig_z > 0:
+	if orig_minus_cur_x * out_minus_orig_x + orig_minus_cur_y * out_minus_orig_y + orig_minus_cur_z * out_minus_orig_z > 0.0:
 		cur_velocity[0].x = 0.0
 		cur_velocity[0].y = 0.0
 		cur_velocity[0].z = 0.0
